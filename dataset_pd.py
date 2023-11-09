b0970c885daf02987b2b3c412387f85a24c87c44
Prawesh Dahal <praweshd@yahoo.com>
2023-11-09 13:11:37 -0500

    dataset creation

--- dataset_pd.py
+++ dataset_pd.py
@@ -1,6 +1,33 @@
 ## Dataset for ICASSP 
 
-import numpy as np 
+import torch 
+from torch.utils.data import ConcatDataset, Dataset
+import torchvision.transforms as transforms 
+import numpy as np
+import os 
 
-x = np.zeros(5)
+#path = '/Users/praweshd/Documents/ICASSP-2024-auditory-eeg-challenge/data/split_data'
+
+# Extract just the EEG training file paths 
+train_eeg = [os.path.join(path,file) for file in os.listdir(path) if "train" in file and "eeg" in file]
+ 
+# Example file 
+data = np.load(train_eeg[0])
+# each eeg dim is shape: (47964, 64)
+
+class Dataset_EEG(Dataset): 
+
+
+    def __init__(self, path = '/Users/praweshd/Documents/ICASSP-2024-auditory-eeg-challenge/data/split_data', file_type = ["train"], frame_length = 64, hop_length = 32):
+        super().__init__()
+        self.frame_length = frame_length
+        self.hop_length = hop_length   
+        self.input_paths = [os.path.join(path,file) for  file in os.listdir(path) if file_type[0] in file and "eeg" in file]
+
+
+    
+    
+    
+
+
  
