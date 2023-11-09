863b123d55ea3b286ada20fd59c4a89f09611b26
Prawesh Dahal <praweshd@yahoo.com>
2023-11-09 14:37:48 -0500

    dataset creation

--- dataset_pd.py
+++ dataset_pd.py
@@ -1,4 +1,5 @@
 ## Dataset for ICASSP 
+# Prawesh Dahal
 
 import torch 
 from torch.utils.data import ConcatDataset, Dataset
@@ -15,17 +16,44 @@ import os
 #data = np.load(train_eeg[0])
 # each eeg dim is shape: (47964, 64)
 
-class Dataset_EEG(Dataset): 
+#Data Initialization that creates Xdata samples
+class Dataset_EEG(Dataset):  
 
-
-    def __init__(self, path = '/Users/praweshd/Documents/ICASSP-2024-auditory-eeg-challenge/data/split_data', file_type = ["train"], frame_length = 64, hop_length = 32):
+    def __init__(self, path = '/Users/praweshd/Documents/ICASSP-2024-auditory-eeg-challenge/data/split_data', file_type = ["train"], frame_length = 64, hop_length = 30):
         super().__init__()
         self.frame_length = frame_length
         self.hop_length = hop_length   
         self.input_paths = [os.path.join(path,file) for  file in os.listdir(path) if file_type[0] in file and "eeg" in file]
+        
+        cnt = 0
+        self.fileidx = {}
 
         for file in self.input_paths: 
             
-            data_shape = np.load(file, mmap_mode= 'r') 
-            # mmap - access small portion of eegfile w/o reading into memory
+            data = np.load(file, mmap_mode= 'r')  # mmap - access small portion of eegfile w/o reading into memory
+            num_samples = data.shape[0]
+            num_windows = 1 +  (num_samples - self.frame_length) // self.hop_length 
+
+            for win in range(num_windows):
+                self.fileidx[cnt] = (file, win)
+                cnt+= 1 
+
+    def __len__(self):
+        return len(self.fileidx)
+
+    def __getitem__(self,idx): 
+        file, win = self.fileidx[idx]
+        start = win*self.hop_length
+        stop = start + self.frame_length 
+        Xdata = np.load(file,mmap_mode='r')[start : stop]
+        return torch.from_numpy(Xdata.copy())
+    
+
+
+
+
+
             
+        
+
+
